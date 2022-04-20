faa94e0a50ee663027a4b5e3f3cb1e151e1d25a7
Tim Cuthbertson <tim@gfxmonk.net>
2022-04-20 21:39:59 +1000

    fix bootstrap test

--- integration/bootstrap/bootstrap_test.ts
+++ integration/bootstrap/bootstrap_test.ts
@@ -1,7 +1,10 @@
 import { run } from "../../lib/cmd.ts";
 import withTempDir from '../../lib/fs/with_temp_dir.ts'
+import { sort } from "../../lib/util/collection.ts";
 import { notNull } from "../../lib/util/object.ts";
 import { assertEquals } from "../../test/common.ts";
+
+const readdir = (p: string) => sort(Array.from(Deno.readDirSync(p)).map(entry => entry.name))
 
 Deno.test('bootstrap', () => withTempDir({}, async (dir: string) => {
 	const installation = ((await Deno.readTextFile('README.md'))
@@ -9,8 +12,6 @@ Deno.test('bootstrap', () => withTempDir({}, async (dir: string) => {
 		.find(line => line.startsWith('curl'))
 	)
 	await run(['bash', '-c', notNull(installation)], { cwd: dir })
-	const dirContents = Array.from(Deno.readDirSync(dir))
-	dirContents.sort()
-	assertEquals(dirContents, ['.gitattributes', 'chored', 'choredefs'])
-	assertEquals((await Deno.stat(dir + '/choredefs')).isFile, true)
+	assertEquals(readdir(dir), ['.gitattributes', 'chored', 'choredefs'])
+	assertEquals(readdir(dir + '/choredefs'), ['render.ts'])
 }))
